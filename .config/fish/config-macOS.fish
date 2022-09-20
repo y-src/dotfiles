a1876c00ea4e778ab1d00209c9f61ef64df7d562
Rahul Yadav <rahul44y@gmail.com>
2022-09-20 21:25:03 +0100

    chore(fish): remove OS-related dependencies and add aliases

---
+++ .config/fish/config-macOS.fish
@@ -0,0 +1,4 @@
+#!/opt/homebrew/bin/fish
+
+# iTerm2 shell integration 
+source ~/.iterm2_shell_integration.fish
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
+# remove the fish welcome message
+set fish_greeting ""
+
+# aliases
+alias lla "ll -A"
+alias g git
+
 # add homebrew to $PATH
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
@@ -6,5 +13,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# iTerm2 shell integration 
-source ~/.iterm2_shell_integration.fish
+# run fish script based on operating system name
+switch (uname)
+	case Darwin
+		source (dirname (status --current-filename))/config-macOS.fish
+end
